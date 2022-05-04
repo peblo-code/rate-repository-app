57539617c1a9684473df3d28ba0f170b1b433022
Pablo Benedix <pabling.benedix@gmail.com>
2022-05-04 16:07:16 -0400

    First commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const Main = () => {
+    return(
+        <View>
+            <Text>Rate Repository Aplication</Text>
+            <RepositoryList />
+        </View>
+    )
+}
+
+export default Main;
